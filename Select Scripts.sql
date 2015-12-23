23b50f0798e232c3604330958df4d7e557c177db
Jonathanifpe <jrsap@a.recife.ifpe.edu.br>
2015-12-23 11:42:11 -0200

    Procedures e Views

--- Select Scripts.sql
+++ Select Scripts.sql
@@ -91,3 +91,76 @@ select nome 'Nome da obra', compositor 'Nome do compositor', datadecriacao 'Data
 	where compositor like 'Mozart'
 		or compositor like 'Be%'
 			and datadecriacao like '177%';
+            
+
+
+select mu.nome, en.pais, en.estado, en.cidade, orq.nome, orq.datadecriacao, num.numerocelular, num.numerofixo
+ from musicos mu, endereco en, orquestra orq, numero num
+	where mu.codorquestra = orq.codorquestra
+	and mu.codnumero = num.codnumero
+	and mu.codendereco = en.codendereco
+	and mu.codfuncao in (select codfuncao from funcoes where nome like 'tutti')
+		order by mu.nome;
+        
+        
+create view orquestra_apresentacao as
+   select ap.datadeapresentacao, orq.nome
+		from apresentacao ap, orquestra orq, sinfonia sin
+			where ap.codorquestra = orq.codorquestra and
+				ap.codsinfonia = sin.codsinfonia and
+				ap.codapresentacao in (select codapresentacao from apresentacao where datadeapresentacao like '2016-03%');
+                
+create view funcaoSpalla as                
+	select mu.nome, en.pais, en.estado, en.cidade, orq.datadecriacao
+		from musicos mu, endereco en, orquestra orq, numero num
+			where mu.codorquestra = orq.codorquestra
+				and mu.codnumero = num.codnumero
+				and mu.codendereco = en.codendereco
+				and mu.codfuncao in (select codfuncao from funcoes where codfuncao = 2)
+					order by mu.nome;
+         
+
+delimiter $$
+create procedure apresentacao ()
+	begin
+		select orq.nome, ap.datadeapresentacao, sin.nome
+		from apresentacao ap, orquestra orq, sinfonia sin
+			where ap.codorquestra = orq.codorquestra and
+                orq.codsinfonia = sin.codsinfonia and
+				ap.codapresentacao in (select codapresentacao from apresentacao where datadeapresentacao like '2016-02%');
+	end $$
+delimiter ;
+
+call apresentacao();
+
+
+delimiter $$
+create procedure nomeMaestro ()
+	begin
+		select mu.nome, fun.nome
+		from musicos mu, funcoes fun
+			where mu.codfuncao = fun.codfuncao and
+				mu.codfuncao in (select codfuncao from funcoes where nome like 'Maes%');
+	end $$
+delimiter ;
+
+call NomeMaestro();
+
+
+
+delimiter $$
+create procedure procuraFuncao (in gen varchar (15))
+	begin
+		select mu.nome, fun.nome
+		from musicos mu, funcoes fun
+			where mu.codfuncao = fun.codfuncao and
+				mu.codfuncao in (select codfuncao from funcoes where nome like gen);
+	end $$
+delimiter ;
+
+
+call procuraFuncao('Tut%');
+
+call procuraFuncao('Spal%');
+
+call procuraFuncao('Chefe%');
